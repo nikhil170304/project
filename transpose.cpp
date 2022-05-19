ba658971eb80a5f5bdf5181312cab6ef89a35830
nikhil170304 <102649230+nikhil170304@users.noreply.github.com>
2022-05-19 11:17:29 +0530

    Add files via upload

---
+++ transpose.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char arr[20];
+	for(int i=0;i<11;i++)
+	{
+		cin>>arr[i];
+	}
+	for(int i=0;i<11;i++)
+	{
+		cout<<arr[i];
+	}
+	return 0;
+}
+
